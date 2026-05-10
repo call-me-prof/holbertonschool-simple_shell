f75f5da496cbe4b8838e619203d7ab780f8991d0
call-me-prof <0pFaisal4@hotmail.com>
2026-05-10 09:05:22 +0000

    Initial commit: task 1

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry point for the simple shell
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+
+	return (0);
+}
